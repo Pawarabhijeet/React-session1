289936931ac93b0afa2ee96350a6f85ec3fa27e2
Abhijeet <abhijeetpawar1575@gmail.com>
2026-08-23 17:21:00 +0530

    feat: react fundamentals

---
+++ react-notes/src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as Components from "./Phase-1-Foundations/Module-03-Fundamentals/01-components"
+
+const App = () =>{
+  return <div>
+    {/* <Components.ExportsExamples/> */}
+    {/* <Components.ComponentsExamples/>   */}
+    <Components.CompositionExamples/>
+  </div>
+};
+export default App;
